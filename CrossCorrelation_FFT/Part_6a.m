c269127d5cf0fd31c542aa02a22811f429d86bc1
Mark Kuo <markkuo1999@gmail.com>
2020-12-30 01:19:26 +0800

    Your message about the commit

--- CrossCorrelation_FFT/Part_6a.m
+++
@@ -1,8 +0,0 @@
-Scene = imread('./Scene.bmp');
-Letter_e = imread('./letter_e.bmp');
-Scene = rgb2gray(Scene);
-Letter_e = rgb2gray(Letter_e);
-
-tic
-crr = xcorr2(Scene, Letter_e);
-toc
---
+++ PCA/OptimizeParam_pca.m
@@ -0,0 +1,2 @@
+%displayEigenface(1);
+recog_pca(18);
